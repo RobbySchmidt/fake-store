a57092e722897f9cabc2c121853931ab3c9c47ee
RobbySchmidt <schmidt@rhowerk.de>
2026-02-22 09:34:08 +0100

    working on colors

---
+++ app/components/HeroBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gradient-to-br from-primary via-accent to-secondary">
+    <div class="container mx-auto py-f-64">
+      <div class="xl:w-10/12 mx-auto px-4 space-y-4">
+        <h1 class="text-f-6xl font-bold text-primary-foreground">Fake Store</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,52 +1,55 @@
 <template>
-  <div class="container mx-auto py-12">
-    <div class="xl:w-10/12 mx-auto px-4 space-y-4">
-      <Select v-model="filterValue">
-        <SelectTrigger class="w-[180px]">
-          <SelectValue placeholder="Select a category" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem :value="null">
-              All products
-            </SelectItem>
-            <SelectItem
-              v-if="categories"
-              v-for="cat in filteredCategories" 
-              :value="cat.name">
-              {{ cat.name }}
-            </SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <span class="block">
-        {{ filteredProducts.length }} Products found
-      </span>
-      <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Card
-          v-if="loading" 
-          v-for="i in filteredProducts">
-          <CardHeader>
-            <CardTitle>
-              <Skeleton class="h-4 w-[250px]" />
-            </CardTitle>
-            <CardDescription>
-              <Skeleton class="h-4 w-[100px]" />
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Skeleton class="aspect-square rounded-xl" />
-          </CardContent>
-          <CardFooter>
-            <Skeleton class="h-4 w-[80px]" />
-            <Skeleton class="h-9 w-[130px]" />
-          </CardFooter>
-        </Card>
-        <ProductCard
-          v-else
-          v-if="filteredProducts" 
-          v-for="product in filteredProducts" 
-          :data="product"/>
+  <div>
+    <HeroBanner />
+    <div class="container mx-auto py-12">
+      <div class="xl:w-10/12 mx-auto px-4 space-y-4">
+        <Select v-model="filterValue">
+          <SelectTrigger class="w-[180px]">
+            <SelectValue placeholder="Select a category" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem :value="null">
+                All products
+              </SelectItem>
+              <SelectItem
+                v-if="categories"
+                v-for="cat in filteredCategories" 
+                :value="cat.name">
+                {{ cat.name }}
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <span class="block">
+          {{ filteredProducts.length }} Products found
+        </span>
+        <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+          <Card
+            v-if="loading" 
+            v-for="i in filteredProducts">
+            <CardHeader>
+              <CardTitle>
+                <Skeleton class="h-4 w-[250px]" />
+              </CardTitle>
+              <CardDescription>
+                <Skeleton class="h-4 w-[100px]" />
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Skeleton class="aspect-square rounded-xl" />
+            </CardContent>
+            <CardFooter>
+              <Skeleton class="h-4 w-[80px]" />
+              <Skeleton class="h-9 w-[130px]" />
+            </CardFooter>
+          </Card>
+          <ProductCard
+            v-else
+            v-if="filteredProducts" 
+            v-for="product in filteredProducts" 
+            :data="product"/>
+        </div>
       </div>
     </div>
   </div>
