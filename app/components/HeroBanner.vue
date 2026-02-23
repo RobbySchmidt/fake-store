e0ed1c4998e95b6db41e94ca1f2b319c31699fc7
RobbySchmidt <schmidt@rhowerk.de>
2026-02-23 09:12:36 +0100

    adjusting colors

--- app/components/HeroBanner.vue
+++ app/components/HeroBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gradient-to-br from-primary via-accent to-secondary">
+  <div class="bg-gradient-to-br from-primary to-card-foreground">
     <div class="container mx-auto py-f-64">
       <div class="xl:w-10/12 mx-auto px-4 space-y-4">
         <h1 class="text-f-6xl font-bold text-primary-foreground">Fake Store</h1>
